8dc2167fbf9047f639b20374bd954d79c23c201a
Adit122022 <adity202212@gmail.com>
2026-03-21 23:47:42 +0530

    FIles that are needed

---
+++ resume-builder/server/src/middleware/validation.js
@@ -0,0 +1,35 @@
+const { body, validationResult } = require('express-validator');
+
+exports.validateResume = [
+    body('personalInfo.fullName').notEmpty().withMessage('Full name is required'),
+    body('personalInfo.email').isEmail().withMessage('Valid email is required'),
+    body('title').optional().isString(),
+    
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            });
+        }
+        next();
+    }
+];
+
+exports.validateRegister = [
+    body('name').notEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+    
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            });
+        }
+        next();
+    }
+];
