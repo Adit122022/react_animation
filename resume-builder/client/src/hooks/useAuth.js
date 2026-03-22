75b99c35c9d8316e1ab8f828695531650752d497
Adit122022 <adity202212@gmail.com>
2026-03-22 17:49:15 +0530

    fixed bugs

--- resume-builder/client/src/hooks/useAuth.js
+++ resume-builder/client/src/hooks/useAuth.js
@@ -1,11 +1,12 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 export const useAuth = () => {
   const { user, token, isLoading } = useSelector((state) => state.auth);
-  
+
   const isAuthenticated = !!token && !!user;
-  const isPremium = user?.subscription === 'premium';
-  const canCreateResume = user?.subscription === 'premium' || user?.resumeCount < 3;
+  const isPremium = user?.subscription === "premium";
+  const canCreateResume =
+    user?.subscription === "premium" || user?.resumeCount < 3;
   return {
     user,
     token,
